0973fa44033f690685e00de3c9dfc207f1fa1d48
AJSTL <ashley.meyer@gmail.com>
2020-03-31 23:47:36 -0500

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.Models
+{
+    public class User
+    {
+        private string Username { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+    }
+}
